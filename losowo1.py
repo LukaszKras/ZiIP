8fa771cfe6dffb90fb107de1397562d265bf295b
LukaszKras <66512136+LukaszKras@users.noreply.github.com>
2020-06-26 20:54:04 +0200

    Create losowo1.py

---
+++ losowo1.py
@@ -0,0 +1,16 @@
+import random
+import turtle
+
+
+
+turtle.shape('turtle')
+
+for i in range(100):
+    turtle.left(90 * random.randrange(1, 3, 1))
+
+                
+    turtle.forward(40)
+
+
+
+turtle.done()
