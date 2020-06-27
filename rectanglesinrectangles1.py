bd4a08e6d57c5668b5a049ae52cde529bbcecda9
Sławomir Marczyński <slawek@zut.edu.pl>
2020-06-27 09:03:42 +0200

    Signed-off-by: Sławomir Marczyński <slawek@zut.edu.pl>

--- rectanglesinrectangles1.py
+++ rectanglesinrectangles1.py
@@ -23,9 +23,10 @@ def narysuj_wielobok(wielobok):
     turtle.up()
     turtle.goto(wielobok[0])
     turtle.down()
-    for i in range(n):
-        j = (i + 1) % n
+    for i in range(1, n + 1):
+        j = i % n
         turtle.goto(wielobok[j])
+    
         
 def oblicz_następny_wielobok(wielobok):
     n = len(wielobok)
@@ -38,21 +39,28 @@ def oblicz_następny_wielobok(wielobok):
         nowy_wielobok.append(środek_odcinka)
     return nowy_wielobok
     
-def narysuj_wieloboki(wielobok, liczba_wieloboków):
-    narysuj_wielobok(wielobok)    
-    if liczba_kwadratów > 1:
-        for k in range(liczba_wieloboków - 1):
-            wielobok = oblicz_następny_wielobok(wielobok)
-            narysuj_wielobok(wielobok)
-    
-    
+
+def narysuj_wieloboki(wielobok, liczba_wieloboków_do_narysowania):
+    if liczba_wieloboków_do_narysowania > 0:
+        narysuj_wielobok(wielobok)    
+        nowy_wielobok = oblicz_następny_wielobok(wielobok)
+        narysuj_wieloboki(nowy_wielobok, liczba_wieloboków_do_narysowania - 1)
+        
+
 
 if __name__ == '__main__':
-    
-    liczba_kwadratów = 10
-    
-    a = 300  # długość boku największego kwadratu
-    kwadrat = [(0, 0), (a, 0), (a, a), (0, a)]
+   
+    liczba_kwadratów = 25
+    a = 550  # długość boku największego kwadratu
+    kwadrat = [ (0, 0), (a, 0), (a, a), (0, a) ]
     narysuj_wieloboki(kwadrat, liczba_kwadratów)
     
+    turtle.color('red')
+    
+    liczba_pięciokątów = 25
+    bok = 350
+    pięciokąt = [ (-bok, -bok), (bok/2, bok), (bok, bok/2),
+                 (2*bok, bok*2), (2.5*bok, bok/1.5)]
+    narysuj_wieloboki(pięciokąt, liczba_pięciokątów)
+
     turtle.done()
